6ce1c1f541f9693ba919953cc01f362881165522
Bagas Arisandi Hidayat <bagas.arisandi@zettacamp.pro>
2022-10-17 20:30:05 +0700

    Final

--- express.js
+++ express.js
@@ -15,7 +15,7 @@ let amDiscount, priceDiscount, amTax ,priceTax, totalPrice, totalPricePur, actua
 // middleware
 app.use(authentication);
 
-
+// route
 app.get('/', (req,res) => {
     res.send(`Welcome admin ${userSet}!`);
 });
@@ -27,7 +27,6 @@ app.get('/book', (req,res) => {
 app.get('/async', async(req,res) => {
     const getPurchaseBook = await purchaseBook(book, 7);
     res.send(bookCredit);
-    //res.send(toc);
 });
 
 app.get('*', (req,res) => {
@@ -39,9 +38,58 @@ app.listen(port);
 console.log(`Server running at port:${port}`);
 
 
-function calculateToc(book){
 
+
+async function termOfCredit(credit, addPrice = 100){
+
+    // array untuk bulan
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"];
+    let currMonth = 9;
+        
+    console.log(`Rincian cicilan ${credit} bulan, dimulai dari bulan ${months[currMonth]} sampai ${totalPricePur}`);
+
+    // harga cicilan/bulan berdasarkan harga buku terakhir
+    let creditPrice = totalPricePur / credit;
+
+    // var kosong untuk update harga total
+    let tocCr = 0;
+    let toc = [];
+
+    // implement desctructuring dan spread
+    const [...a] = months;
+
+    // looping for pushing the 
+    for (let i = 0; i < credit; i++){
+        
+        if(i === 5 ){
+            creditPrice += addPrice ;
+        }
+        
+        tocCr += creditPrice;
+
+        // control month berdasarkan bulan sekarang
+        if (currMonth > 11){
+            currMonth = 0;
+            }
+
+            // array func, untuk push ke array object
+            toc.push( {
+            bulan: a[currMonth],
+            cicilan: Math.round(creditPrice),
+            total: Math.round(tocCr)
+            } );
+
+                
+        currMonth++;
+            };
+
+            console.log(toc);
+            return toc;
+    }
+
+function calculateToc(book){
     // calculate of certain price
+    
     //discount
     amDiscount = book.discount / 100 * book.price;
     priceDiscount = book.price - amDiscount;
@@ -52,76 +100,29 @@ function calculateToc(book){
     totalPrice = book.price + amTax - amDiscount;
     }
 
-async function termOfCredit(credit, addPrice = 100){
+async function purchaseBook(book,credit) {
 
-         // array untuk bulan
-        const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"];
-        let currMonth = 9;
-            
-        console.log(`Rincian cicilan ${credit} bulan, dimulai dari bulan ${months[currMonth]} sampai ${totalPricePur}`);
-    
-        // harga cicilan/bulan berdasarkan harga buku terakhir
-        let creditPrice = totalPricePur / credit;
-
-        // var kosong untuk update harga total
-        let tocCr = 0;
-        let toc = [];
-    
-        // implement desctructuring dan spread
-        const [...a] = months;
-    
-        // looping for pushing the 
-        for (let i = 0; i < credit; i++){
-            
-            if(i === 5 ){
-                creditPrice += addPrice ;
-            }
-            
-            tocCr += creditPrice;
-
-            // control month berdasarkan bulan sekarang
-            if (currMonth > 11){
-                currMonth = 0;
-                }
-    
-                // array func, untuk push ke array object
-             toc.push( {
-                bulan: a[currMonth],
-                cicilan: Math.round(creditPrice),
-                total: Math.round(tocCr)
-                } );
-    
-                    
-            currMonth++;
-                };
-    
-                console.log(toc);
-                return toc;
-    }
-
-    async function purchaseBook(book,credit) {
+    // Jika onsale true
+    if (book.status == true){
         calculateToc(book);
-    
-        // Jika onsale true
-        if (book.status == true){
-            // looping untuk pembeian
-            for(let i = 1; i <= book.purchase; i++){
-                // console.log("Barang dibeli",i);
-                totalPricePur = totalPrice * i;
-                actualPur++;
-                book.stock--;
-    
-                if (book.stock < 1){
-                    // console.log("=> Barang habis");
-                    stock = 0;
-                    break;
-                }
-            };
+
+        // looping untuk pembeian
+        for(let i = 1; i <= book.purchase; i++){
+            // console.log("Barang dibeli",i);
+            totalPricePur = totalPrice * i;
+            actualPur++;
+            book.stock--;
+
+            if (book.stock < 1){
+                // console.log("=> Barang habis");
+                stock = 0;
+                break;
+            }
+        };
 
         bookCredit = await termOfCredit(credit);
         // console.log(bookCredit);
-
-
+        
         // Jika onsale false
         } else {
             console.log("=> Status buku ini tidak dijual");
@@ -129,26 +130,26 @@ async function termOfCredit(credit, addPrice = 100){
     };
 
     
-    function authentication(req, res, next){
-        let authheader = req.headers.authorization;
-        //console.log(req.headers.authorization);
+function authentication(req, res, next){
+    let authheader = req.headers.authorization;
+    //console.log(req.headers.authorization);
+
+    if (!authheader){
+        res.send("Tidak ada otentikasi..");
+        res.end();
+    } else {
+        let auth = new Buffer.from(authheader.split(' ')[1],'base64').toString().split(':');
+        let user = auth[0];
+        let pass = auth[1];
+        //console.log(auth)
+        if (user == userSet && pass == passSet) {
     
-        if (!authheader){
-            res.send("Tidak ada otentikasi..");
-            res.end();
+            // If Authorized user
+            // console.log(`${userSet} berhasil terotentikasi..`);
+            next();
         } else {
-            let auth = new Buffer.from(authheader.split(' ')[1],'base64').toString().split(':');
-            let user = auth[0];
-            let pass = auth[1];
-            //console.log(auth)
-            if (user == userSet && pass == passSet) {
-     
-                // If Authorized user
-                // console.log(`${userSet} berhasil terotentikasi..`);
-                next();
-            } else {
-                res.send("Kamu tidak terotentikasi..");
-                res.end();
-            }
+            res.send("Kamu tidak terotentikasi..");
+            res.end();
         }
     }
+}
--- test.js
+++ test.js
@@ -1,15 +1,157 @@
-function x(){
-    let book ={
-        nama: "asdwa",
-        price: 2211 
+const express = require("express");
+const book = require('./app.js')
+
+const app = express();
+const port = 4000;
+
+let bookCredit = [];
+
+const userSet = 'bagas';
+const passSet = 'admin123';
+
+// set variabel for calculating later
+let amDiscount, priceDiscount, amTax ,priceTax, totalPrice, totalPricePur, actualPur = 0;
+
+// middleware
+app.use(authentication);
+
+// route
+app.get('/', (req,res) => {
+    res.send(`Welcome admin ${userSet}!`);
+});
+
+app.get('/book', (req,res) => {
+    res.send(book);
+ });
+
+app.get('/async', async(req,res) => {
+    const getPurchaseBook = await purchaseBook(book, 7);
+    res.send(bookCredit);
+    //res.send(toc);
+});
+
+app.get('*', (req,res) => {
+    res.send("Path tidak ditemukan..");
+ });
+
+
+app.listen(port);
+console.log(`Server running at port:${port}`);
+
+
+
+
+async function termOfCredit(credit, addPrice = 100){
+
+         // array untuk bulan
+        const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"];
+        let currMonth = 9;
+            
+        console.log(`Rincian cicilan ${credit} bulan, dimulai dari bulan ${months[currMonth]} sampai ${totalPricePur}`);
+    
+        // harga cicilan/bulan berdasarkan harga buku terakhir
+        let creditPrice = totalPricePur / credit;
+
+        // var kosong untuk update harga total
+        let tocCr = 0;
+        let toc = [];
+    
+        // implement desctructuring dan spread
+        const [...a] = months;
+    
+        // looping for pushing the 
+        for (let i = 0; i < credit; i++){
+            
+            if(i === 5 ){
+                creditPrice += addPrice ;
+            }
+            
+            tocCr += creditPrice;
+
+            // control month berdasarkan bulan sekarang
+            if (currMonth > 11){
+                currMonth = 0;
+                }
+    
+                // array func, untuk push ke array object
+             toc.push( {
+                bulan: a[currMonth],
+                cicilan: Math.round(creditPrice),
+                total: Math.round(tocCr)
+                } );
+    
+                    
+            currMonth++;
+                };
+    
+                console.log(toc);
+                return toc;
     }
-return book;
-}
 
-async function tampilx(){
-    const xVar = await x();
-   // console.log(xVar);
-}
+function calculateToc(book){
+    // calculate of certain price
+    
+    //discount
+    amDiscount = book.discount / 100 * book.price;
+    priceDiscount = book.price - amDiscount;
+    // tax
+    amTax = book.tax / 100 * book.price;
+    priceTax = book.price + amTax;
+    // Total price after tax and discount
+    totalPrice = book.price + amTax - amDiscount;
+    }
 
-const tampil = tampilx()
-console.log(tampil)
+async function purchaseBook(book,credit) {
+
+    // Jika onsale true
+    if (book.status == true){
+        calculateToc(book);
+        
+        // looping untuk pembeian
+        for(let i = 1; i <= book.purchase; i++){
+            // console.log("Barang dibeli",i);
+            totalPricePur = totalPrice * i;
+            actualPur++;
+            book.stock--;
+
+            if (book.stock < 1){
+                // console.log("=> Barang habis");
+                stock = 0;
+                break;
+            }
+        };
+
+        bookCredit = await termOfCredit(credit);
+        // console.log(bookCredit);
+
+
+        // Jika onsale false
+        } else {
+            console.log("=> Status buku ini tidak dijual");
+        };
+    };
+
+    
+function authentication(req, res, next){
+    let authheader = req.headers.authorization;
+    //console.log(req.headers.authorization);
+
+    if (!authheader){
+        res.send("Tidak ada otentikasi..");
+        res.end();
+    } else {
+        let auth = new Buffer.from(authheader.split(' ')[1],'base64').toString().split(':');
+        let user = auth[0];
+        let pass = auth[1];
+        //console.log(auth)
+        if (user == userSet && pass == passSet) {
+    
+            // If Authorized user
+            // console.log(`${userSet} berhasil terotentikasi..`);
+            next();
+        } else {
+            res.send("Kamu tidak terotentikasi..");
+            res.end();
+        }
+    }
+}
