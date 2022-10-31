d0f4df5d42e4b3a1a46f1161ce42631de4bdf2fd
Bagas Arisandi Hidayat <bagas.arisandi@zettacamp.pro>
2022-10-31 16:54:30 +0700

    2nd Commit

--- models/songlist.js
+++ models/songlist.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 
 const songListModel = new mongoose.Schema({ 
-    title: String,
-    artist: String,
-    genre: String,
-    duration: String
+    name: String,
+    list: String,
+    total_duration: String
 }, {timestamps: true});
 
-const songlist = mongoose.model('songlist', songModel);
+const songlist = mongoose.model('songlists', songListModel);
 module.exports = songlist;
